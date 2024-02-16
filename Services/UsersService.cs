d94f5971d516b1a7992107e8bbfb67f0c85b521f
Sorokovsky <Sorokovskys@ukr.net>
2024-02-16 14:04:51 +0200

    refactor

--- Services/UsersService.cs
+++ Services/UsersService.cs
@@ -7,6 +7,7 @@ namespace SorokChatServer.Services
     public class UsersService
     {
         private readonly IUsersRepository _userRepository;
+        private readonly Func<UsersEntity, bool> _allUser = user => true;
 
         public UsersService(IUsersRepository userRepository)
         {
@@ -15,7 +16,7 @@ namespace SorokChatServer.Services
 
         public List<UsersEntity> GetAll()
         {
-            List<UsersEntity> users = _userRepository.Find(user => true);
+            List<UsersEntity> users = _userRepository.Find(_allUser);
             if(users.Count == 0)
             {
                 throw new BadHttpRequestException("Users not found");
@@ -25,7 +26,7 @@ namespace SorokChatServer.Services
 
         public UsersEntity GetById(long id)
         {
-            List<UsersEntity> users = _userRepository.Find(user => user.Id == id);
+            List<UsersEntity> users = _userRepository.Find(GetByIdPredicate(id));
             if(users.Count < 1)
             {
                 throw new BadHttpRequestException($"User with id = {id} not found");
@@ -47,6 +48,11 @@ namespace SorokChatServer.Services
         public UsersEntity Delete(long id)
         {
             return _userRepository.Delete(new UsersEntity(id));
+        }
+
+        private Func<UsersEntity, bool> GetByIdPredicate(long id)
+        {
+            return user => user.Id == id;
         }
     }
 }
