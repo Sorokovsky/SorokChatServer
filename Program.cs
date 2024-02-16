ef114b285b637a791c80f92f763eece5216c1297
Sorokovsky <Sorokovskys@ukr.net>
2024-02-16 19:57:37 +0200

    jwt auth support

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using SorokChatServer.Database.Context;
 using SorokChatServer.Database.Repositories;
 using SorokChatServer.Services;
+using System.Text;
 
 internal class Program
 {
@@ -14,6 +17,24 @@ internal class Program
         builder.Services.AddSingleton(typeof(ChatContext));
         builder.Services.AddScoped<IUsersRepository, UsersRepository>();
         builder.Services.AddScoped(typeof(UsersService));
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                RequireExpirationTime = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"], // Ваше видавництво (видавець) JWT
+                ValidAudience = builder.Configuration["Jwt:Audit"], // Ваше аудиторія
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:SecretKey"]))
+            };
+        });
         WebApplication app = builder.Build();
 
         if (app.Environment.IsDevelopment())
