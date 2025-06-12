4a9e780b36726f09c82de17f7e44e3574643a8c6
Sorokovsky <Sorokovskys@ukr.net>
2025-06-12 17:15:46 +0300

    start

---
+++ SorokChatServer.Application/Database/Entities/UserEntity.cs
@@ -0,0 +1,25 @@
+﻿namespace SorokChatServer.Application.Database.Entities;
+
+public class UserEntity : BaseEntity
+{
+    public UserEntity(long id, DateTime createdAt, DateTime updatedAt, string firstName, string lastName, string email, string password) 
+        : base(id, createdAt, updatedAt)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        Password = password;
+    }
+
+    public UserEntity()
+    {
+    }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string Password { get; set; }
+}
---
+++ SorokChatServer.Application/Database/SorokChatDatabaseContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SorokChatServer.Application.Database.Entities;
+
+namespace SorokChatServer.Application.Database;
+
+public class SorokChatDatabaseContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public SorokChatDatabaseContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    
+    public DbSet<UserEntity> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder builder)
+    {
+        builder
+            .UseNpgsql(_configuration.GetConnectionString(nameof(SorokChatDatabaseContext)))
+            .UseSnakeCaseNamingConvention();
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.ApplyConfigurationsFromAssembly(typeof(SorokChatDatabaseContext).Assembly);
+    }
+}
