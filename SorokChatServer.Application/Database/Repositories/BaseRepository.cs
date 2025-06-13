a28f198b26b21d88d424ee56152a2fb04107f0ae
Sorokovsky <Sorokovskys@ukr.net>
2025-06-13 19:55:18 +0300

    repository get and create

--- SorokChatServer.Application/Database/Repositories/BaseRepository.cs
+++ SorokChatServer.Application/Database/Repositories/BaseRepository.cs
@@ -1,12 +1,12 @@
 ﻿using System.Linq.Expressions;
 using CSharpFunctionalExtensions;
 using Microsoft.EntityFrameworkCore;
+using SorokChatServer.Application.Database.Entities;
 using SorokChatServer.Application.Interfaces;
-using SorokChatServer.Application.Models;
 
 namespace SorokChatServer.Application.Database.Repositories;
 
-public abstract class BaseRepository<T> : IRepository<T> where T : Base
+public abstract class BaseRepository<T> : IRepository<T> where T : BaseEntity
 {
     private readonly SorokChatDatabaseContext _context;
     private readonly DbSet<T> _set;
@@ -17,22 +17,45 @@ public abstract class BaseRepository<T> : IRepository<T> where T : Base
         _set = _context.Set<T>();
     }
 
-    public async Task<List<T>> GetMany(Expression<Func<T, bool>> predicate)
+    public async Task<List<T>> GetMany(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
     {
-        return await _set.AsNoTracking().Where(predicate).ToListAsync();
+        return await _set.AsNoTracking().Where(predicate).ToListAsync(cancellationToken);
     }
 
-    public async Task<Result<T, string>> FindOne(Expression<Func<T, bool>> predicate)
+    public async Task<Result<T, string>> GetOne(Expression<Func<T, bool>> predicate,
+        CancellationToken cancellationToken)
     {
-        var candidate = await _set.AsNoTracking().FirstOrDefaultAsync(predicate);
+        var candidate = await _set.AsNoTracking().FirstOrDefaultAsync(predicate, cancellationToken);
         var errorResult = Result.Failure<T, string>($"{typeof(T).Name} not found.");
         var successResult = Result.Success<T, string>(candidate!);
         return candidate is null ? errorResult : successResult;
     }
 
-    public abstract Task<Result<T, string>> Create(T item);
+    public async Task<Result<T, string>> Create(T item, CancellationToken cancellationToken)
+    {
+        await using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken);
+        try
+        {
+            var created = (await _set.AddAsync(item, cancellationToken)).Entity;
+            await _context.SaveChangesAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
+            return Result.Success<T, string>(created);
+        }
+        catch (Exception e)
+        {
+            await transaction.RollbackAsync(cancellationToken);
+            return Result.Failure<T, string>(e.Message);
+        }
+    }
 
-    public abstract Task<Result<T, string>> Update(Expression<Func<T, bool>> predicate, T updated);
+    public async Task<Result<T, string>> Update(Expression<Func<T, bool>> predicate, T updated,
+        CancellationToken cancellationToken)
+    {
+        var candidateResult = await GetOne(predicate, cancellationToken);
+        if (candidateResult.IsFailure) return candidateResult;
+        throw new NotImplementedException();
+    }
 
-    public abstract Task<Result<T, string>> Delete(Expression<Func<T, bool>> predicate);
+    public abstract Task<Result<T, string>> Delete(Expression<Func<T, bool>> predicate,
+        CancellationToken cancellationToken);
 }
--- SorokChatServer.Application/Interfaces/IRepository.cs
+++ SorokChatServer.Application/Interfaces/IRepository.cs
@@ -1,18 +1,19 @@
 ﻿using System.Linq.Expressions;
 using CSharpFunctionalExtensions;
-using SorokChatServer.Application.Models;
+using SorokChatServer.Application.Database.Entities;
 
 namespace SorokChatServer.Application.Interfaces;
 
-public interface IRepository<T> where T : Base
+public interface IRepository<T> where T : BaseEntity
 {
-    public Task<List<T>> GetMany(Expression<Func<T, bool>> predicate);
+    public Task<List<T>> GetMany(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken);
 
-    public Task<Result<T, string>> FindOne(Expression<Func<T, bool>> predicate);
+    public Task<Result<T, string>> GetOne(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken);
 
-    public Task<Result<T, string>> Create(T item);
+    public Task<Result<T, string>> Create(T item, CancellationToken cancellationToken);
 
-    public Task<Result<T, string>> Update(Expression<Func<T, bool>> predicate, T updated);
+    public Task<Result<T, string>> Update(Expression<Func<T, bool>> predicate, T updated,
+        CancellationToken cancellationToken);
 
-    public Task<Result<T, string>> Delete(Expression<Func<T, bool>> predicate);
+    public Task<Result<T, string>> Delete(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken);
 }
