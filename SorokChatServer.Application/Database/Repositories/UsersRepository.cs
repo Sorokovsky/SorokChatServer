e4695e92ba2aaa87b5d6c88a7ecd4a95e6bd69cc
Sorokovsky <Sorokovskys@ukr.net>
2025-06-13 22:05:42 +0300

    users service

---
+++ SorokChatServer.Application/Database/Repositories/UsersRepository.cs
@@ -0,0 +1,21 @@
+﻿using CSharpFunctionalExtensions;
+using SorokChatServer.Application.Database.Entities;
+
+namespace SorokChatServer.Application.Database.Repositories;
+
+public class UsersRepository : BaseRepository<UserEntity>
+{
+    public UsersRepository(SorokChatDatabaseContext context) : base(context)
+    {
+    }
+
+    public async Task<Result<UserEntity, string>> GetOneById(long id, CancellationToken cancellationToken)
+    {
+        return await GetOne(x => x.Id == id, cancellationToken);
+    }
+
+    public async Task<Result<UserEntity, string>> GetOneByEmail(string email, CancellationToken cancellationToken)
+    {
+        return await GetOne(x => x.Email == email, cancellationToken);
+    }
+}
