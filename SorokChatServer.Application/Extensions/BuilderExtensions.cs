4d96dd414f77d1a68f8cbd7fc45f608533dd66d9
Sorokovsky <Sorokovskys@ukr.net>
2025-06-14 12:16:08 +0300

    refactor

---
+++ SorokChatServer.Application/Extensions/BuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using SorokChatServer.Application.Database.Repositories;
+using SorokChatServer.Application.Interfaces;
+using SorokChatServer.Application.Services;
+
+namespace SorokChatServer.Application.Extensions;
+
+public static class BuilderExtensions
+{
+    public static void AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IUsersRepository, UsersRepository>();
+    }
+
+    public static void AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUsersService, UsersService>();
+        services.AddSingleton<IPasswordService, BcryptPasswordService>();
+    }
+
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen();
+        services.AddOpenApi();
+    }
+
+    public static void UseSwaggerView(this WebApplication application)
+    {
+        application.MapOpenApi();
+        application.UseSwagger();
+        application.UseSwaggerUI();
+    }
+}
